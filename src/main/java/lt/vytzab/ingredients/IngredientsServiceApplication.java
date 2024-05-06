45b78e59dc7e6165bb8ad39c01462e336c9f9dd1
vytzab <vytautas.zabielskas@gmail.com>
2024-05-06 09:18:11 +0300

    Initial commit

---
+++ src/main/java/lt/vytzab/ingredients/IngredientsServiceApplication.java
@@ -0,0 +1,13 @@
+package lt.vytzab.ingredients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngredientsServiceApplication.class, args);
+	}
+
+}
